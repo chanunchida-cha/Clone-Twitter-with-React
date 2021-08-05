4e0a4ed3afb8f3f91dddc0d00db7c985a2506b6c
Chanunchida <yok.8.12.2543@gmail.com>
2021-08-05 13:15:10 +0700

    add function tweet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react'
-import  './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import bgImage from "./img/bg.png";
 
 export default function App() {
+  //states
+  const [noteContent, setNoteContent] = useState("");
+  const [allNotes, setAllNotes] = useState([]);
+
+
+  //Functions
+  //function Tweet
+  function onNoteSubmit(event) {
+    event.preventDefault();
+   setAllNotes((prevAllNotes)=>{
+     return [...prevAllNotes,noteContent]
+   })
+
+  }
+
+  //Elements
+  const noteElements = allNotes.map((note,index) => {
+    return(
+    <div key={index} className="app-newcontent">
+      {note}
+    </div>
+    )
+  });
+
+
   return (
-    <center>
-    <section className="app-section" >
-      <div className ="app-container">
-        <header className="app-header">
-          Twitty
-        </header>
-        <div className="input-text">
-          <textarea type="text" placeholder="what's happening?" className="input"></textarea>
-
+    <div className="app-bg">
+      <center>
+        <div className="app-container">
+          <form onSubmit={onNoteSubmit}>
+            <div className="app-header">Twitty</div>
+            <div className="input-text">
+              <textarea
+                rows="4"
+                type="text"
+                placeholder="what's happening?"
+                className="input"
+                value={noteContent}
+                onChange={(event) => {
+                  setNoteContent(event.target.value);
+                }}
+              ></textarea>
+            </div>
+            <div className="app-button">
+              <button className="tweet-button" type="submit">
+                Tweet
+              </button>
+            </div>
+          </form>
+          <div className="app-notes">{noteElements}</div>
         </div>
-        <div className="app-button">
-          <button className="tweet-button">Tweet</button>
-        </div>
-
-      </div>
-
-    </section>
-    </center>
-  )
+      </center>
+    </div>
+  );
 }
-
